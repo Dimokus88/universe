201a11e56f395bfef7a56e81a6fd1fd543726d03
Dimokus <Dimokus@mail.ru>
2022-07-23 15:00:42 +0300

    Create reward.sh

---
+++ script/reward.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# By Dimokus (https://t.me/Dimokus)
+if [[ `echo $reward` -gt 1000000 ]]
+	then
+		echo =============================================================
+		echo ============Rewards discovered, collecting...================
+		echo =============================================================
+		echo =============================================================
+		echo =============Обнаружены награды, собираю...==================
+		echo =============================================================
+		(echo ${PASSWALLET}) | $binary tx distribution withdraw-rewards $valoper --from $address --gas="auto" --fees 5555$denom --commission -y
+		reward=0
+		sleep 5
+	fi
