3f1a417addc0e12607a5853b4ba74804089bde1e
Dimokus <Dimokus@mail.ru>
2022-07-23 16:49:28 +0300

    Update cbinary.sh

--- script/cbinary.sh
+++ script/cbinary.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # By Dimokus (https://t.me/Dimokus)
+ver="1.18.1" && \
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+sudo rm -rf /usr/local/go && \
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+rm "go$ver.linux-amd64.tar.gz" && \
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
+source $HOME/.bash_profile && \
+go version
+mkdir -p /root/go/bin/
 git clone $gitrep && cd $gitfold
 echo $vers
 sleep 5
