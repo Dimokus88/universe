ddff4ebb5a2e007baaec31b263ce2414345af019
Dimokus <Dimokus@mail.ru>
2022-07-23 15:08:03 +0300

    Update GoSetup.sh

--- script/GoSetup.sh
+++ script/GoSetup.sh
@@ -8,3 +8,4 @@ rm "go$ver.linux-amd64.tar.gz" && \
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
 source $HOME/.bash_profile && \
 go version
+mkdir -p /root/go/bin/
