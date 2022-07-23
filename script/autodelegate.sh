8b95c79a88993f6023a98b6af74df4b53d0162eb
Dimokus <Dimokus@mail.ru>
2022-07-23 14:54:53 +0300

    Create autodelegate.sh

---
+++ script/autodelegate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# By Dimokus (https://t.me/Dimokus)
+if [[ $autodelegate == yes ]]
+	then
+		balance=`$binary q bank balances $address -o json | jq -r .balances[].amount `
+		balance=`printf "%.f \n" $balance`
+		echo =================================================
+		echo ===============Balance check...==================
+		echo =================================================
+		echo =================================================
+		echo =============Проверка баланса...=================
+		echo =================================================
+		echo =========================
+		echo ==Ваш баланс: $balance ==
+		echo = Your balance $balance =
+		echo =========================
+		sleep 5
+		if [[ `echo $balance` -gt 1000000 ]]
+		then
+			echo ======================================================================
+			echo ============Balance = $balance . Delegate to validator================
+			echo ======================================================================
+			echo ======================================================================
+			echo =============Баланс = $balance . Делегирую валидатору=================
+			echo ======================================================================
+			stake=$(($balance-500000))
+			(echo ${PASSWALLET}) | $binary tx staking delegate $valoper ${stake}`echo $denom` --from $address --chain-id $chain --gas="auto" --fees 5555$denom -y
+			sleep 5
+			stake=0
+			balance=0
+		fi
+	else	
+		echo ===========================================================
+		echo =============== auto-delegation disabled ==================
+		echo ===============автоделегирование отключено=================
+		echo ===========================================================
+	fi
