8aa639770ddfb5c78d75723a6bbfdd6a334cabb3
Dimokus <Dimokus@mail.ru>
2022-07-23 15:10:51 +0300

    Create addwallet.sh

---
+++ script/addwallet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# By Dimokus (https://t.me/Dimokus)
+(echo "${MNEMONIC}"; echo ${PASSWALLET}; echo ${PASSWALLET}) | $binary keys add ${WALLET_NAME} --recover
+address=`(echo ${PASSWALLET}) | $(which $binary) keys show $WALLET_NAME -a`
+valoper=`(echo ${PASSWALLET}) | $(which $binary) keys show $WALLET_NAME  --bech val -a`
+echo =====Ваш адрес =====
+echo ===Your address ====
+echo $address
+echo ==========================
+echo =====Your valoper=====
+echo ======Ваш valoper=====
+echo $valoper
+echo ===========================
