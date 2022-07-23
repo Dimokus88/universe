cdf0cf1cdb9e7af50b49a06b5aec7eb36d246740
Dimokus <Dimokus@mail.ru>
2022-07-23 17:02:03 +0300

    Update cbinary.sh

--- script/cbinary.sh
+++ script/cbinary.sh
@@ -8,6 +8,8 @@ rm "go$ver.linux-amd64.tar.gz" && \
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
 source $HOME/.bash_profile && \
 go version
+echo $GOPATH  # this should point to your workspace
+echo $PATH
 mkdir -p /root/go/bin/
 cd /root/
 git clone $gitrep && cd $gitfold
