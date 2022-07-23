184f0ce8482c39f71a6f8883d80daf2ddb85913a
Dimokus <Dimokus@mail.ru>
2022-07-23 15:17:11 +0300

    Create run_at_service.sh

---
+++ script/run_at_service.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# By Dimokus (https://t.me/Dimokus)
+echo =Run node...=
+mkdir /root/$binary
+mkdir /root/$binary/log
+
+cat > /root/$binary/run <<EOF 
+#!/bin/bash
+exec 2>&1
+exec $binary start
+EOF
+
+chmod +x /root/$binary/run
+LOG=/var/log/$binary
+
+cat > /root/$binary/log/run <<EOF 
+#!/bin/bash
+mkdir $LOG
+exec svlogd -tt $LOG
+EOF
+
+chmod +x /root/$binary/log/run
+ln -s /root/$binary /etc/service
