6995795b54722d22c6b8b55b2830c68ed6a59b1c
Dimokus <Dimokus@mail.ru>
2022-07-23 16:02:21 +0300

    Update work.sh

--- script/work.sh
+++ script/work.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 	sleep 5m
 	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/info.sh | bash
+	source $HOME/.bash_profile
 	#===============СБОР НАГРАД И КОМИССИОННЫХ===================
 	reward=`$binary query distribution rewards $address $valoper -o json | jq -r .rewards[].amount`
 	reward=`printf "%.f \n" $reward`
@@ -8,14 +9,18 @@
 	echo ==Ваши награды: $reward $denom==
 	echo ===Your reward $reward $denom===
 	echo ==============================
+	source $HOME/.bash_profile
 	sleep 5
-	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/reward.sh | bash	
+	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/reward.sh | bash
+	source $HOME/.bash_profile
 	#============================================================
 	
 	#+++++++++++++++++++++++++++АВТОДЕЛЕГИРОВАНИЕ++++++++++++++++++++++++
 	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/autodelegate.sh | bash
+	source $HOME/.bash_profile
 	#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	
 	#--------------------------ВЫХОД ИЗ ТЮРЬМЫ--------------------------
 	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/unjail.sh | bash
+	source $HOME/.bash_profile
 	#-------------------------------------------------------------------
