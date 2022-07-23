bfafa4847be5b51a13c1aae8cbc49b0dc195ed3f
Dimokus <Dimokus@mail.ru>
2022-07-23 17:27:26 +0300

    Update work.sh

--- script/work.sh
+++ script/work.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 	sleep 5m
 	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/info.sh | bash
-	source $HOME/.bash_profile
+	source ~/.profile
+
 	#===============СБОР НАГРАД И КОМИССИОННЫХ===================
 	reward=`$binary query distribution rewards $address $valoper -o json | jq -r .rewards[].amount`
 	reward=`printf "%.f \n" $reward`
@@ -9,18 +10,22 @@
 	echo ==Ваши награды: $reward $denom==
 	echo ===Your reward $reward $denom===
 	echo ==============================
-	source $HOME/.bash_profile
+	source ~/.profile
+
 	sleep 5
 	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/reward.sh | bash
-	source $HOME/.bash_profile
+	source ~/.profile
+
 	#============================================================
 	
 	#+++++++++++++++++++++++++++АВТОДЕЛЕГИРОВАНИЕ++++++++++++++++++++++++
 	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/autodelegate.sh | bash
-	source $HOME/.bash_profile
+	source ~/.profile
+
 	#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	
 	#--------------------------ВЫХОД ИЗ ТЮРЬМЫ--------------------------
 	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/unjail.sh | bash
-	source $HOME/.bash_profile
+	source ~/.profile
+
 	#-------------------------------------------------------------------
