e529b4e0e70166801619465664d009e8344484d9
Dimokus <Dimokus@mail.ru>
2022-07-23 19:40:05 +0300

    Update work.sh

--- script/work.sh
+++ script/work.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 	sleep 5m
 	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/info.sh | bash
-	source /etc/environment
+	source $HOME/.bash_profile
 
 	#===============СБОР НАГРАД И КОМИССИОННЫХ===================
 	reward=`$binary query distribution rewards $address $valoper -o json | jq -r .rewards[].amount`
@@ -10,22 +10,21 @@
 	echo ==Ваши награды: $reward $denom==
 	echo ===Your reward $reward $denom===
 	echo ==============================
-	source /etc/environment
-
+	source $HOME/.bash_profile
 	sleep 5
 	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/reward.sh | bash
-	source /etc/environment
+	source $HOME/.bash_profile
 
 	#============================================================
 	
 	#+++++++++++++++++++++++++++АВТОДЕЛЕГИРОВАНИЕ++++++++++++++++++++++++
 	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/autodelegate.sh | bash
-	source /etc/environment
+	source $HOME/.bash_profile
 
 	#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	
 	#--------------------------ВЫХОД ИЗ ТЮРЬМЫ--------------------------
 	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/unjail.sh | bash
-	source /etc/environment
+	source $HOME/.bash_profile
 
 	#-------------------------------------------------------------------
