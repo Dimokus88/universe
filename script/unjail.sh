851c4eb5f83e90544076d6789f60993aea232c26
Dimokus <Dimokus@mail.ru>
2022-07-23 16:02:00 +0300

    Update unjail.sh

--- script/unjail.sh
+++ script/unjail.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # By Dimokus (https://t.me/Dimokus)
 jailed=`$binary query staking validator $valoper -o json | jq -r .jailed`
+source $HOME/.bash_profile
 	while [[  $jailed == true ]] 
 	do
 		echo ==Внимание! Валидатор в тюрьме, попытка выхода из тюрьмы произойдет через 30 минут==
@@ -9,4 +10,5 @@ jailed=`$binary query staking validator $valoper -o json | jq -r .jailed`
 		(echo ${PASSWALLET}) | $binary tx slashing unjail --from $address --chain-id $chain --fees 5000$denom -y
 		sleep 10
 		jailed=`$binary query staking validator $valoper -o json | jq -r .jailed`
+		source $HOME/.bash_profile
 	done
