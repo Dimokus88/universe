482fb82d513f11adcfa2510866dcdea40a1f0696
Dimokus <Dimokus@mail.ru>
2022-07-23 14:59:34 +0300

    Create unjail.sh

---
+++ script/unjail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# By Dimokus (https://t.me/Dimokus)
+jailed=`$binary query staking validator $valoper -o json | jq -r .jailed`
+	while [[  $jailed == true ]] 
+	do
+		echo ==Внимание! Валидатор в тюрьме, попытка выхода из тюрьмы произойдет через 30 минут==
+		echo =Attention! Validator in jail, attempt to get out of jail will happen in 30 minutes=
+		sleep 30m
+		(echo ${PASSWALLET}) | $binary tx slashing unjail --from $address --chain-id $chain --fees 5000$denom -y
+		sleep 10
+		jailed=`$binary query staking validator $valoper -o json | jq -r .jailed`
+	done
