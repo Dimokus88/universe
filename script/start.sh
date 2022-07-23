c30253be3cc5df7074af2e0994f49d2c1a98876f
Dimokus <Dimokus@mail.ru>
2022-07-23 15:22:27 +0300

    Create start.sh

---
+++ script/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# By Dimokus (https://t.me/Dimokus)
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+(echo ${my_root_password}; echo ${my_root_password}) | passwd root
+service ssh restart
+service nginx start
+sleep 5
+sudo apt-get install -y nano runit
+runsvdir -P /etc/service &
