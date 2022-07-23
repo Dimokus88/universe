828726b3bbaaf8960ab59f03b384bbc99cd6ea24
Dimokus <Dimokus@mail.ru>
2022-07-23 15:14:05 +0300

    Create toml_settings.sh

---
+++ script/toml_settings.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# By Dimokus (https://t.me/Dimokus)
+if [[ -n $link_peer ]]
+then
+	PEER=`curl -s $link_peer`
+fi
+if [[ -n $link_seed ]]
+then
+	SEED=`curl -s $link_seed`
+fi
+echo $PEER
+echo $SEED
+sleep 5
+sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0025$denom\"/;" $HOME/$folder/config/app.toml
+sleep 1
+sed -i.bak -e "s/^seeds *=.*/seeds = \"$SEED\"/;" $HOME/$folder/config/config.toml
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEER\"/;" $HOME/$folder/config/config.toml
+sed -i.bak -e "s_"tcp://127.0.0.1:26657"_"tcp://0.0.0.0:26657"_;" $HOME/$folder/config/config.toml
+pruning="custom" && \
+pruning_keep_recent="5" && \
+pruning_keep_every="1000" && \
+pruning_interval="50" && \
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$folder/config/app.toml && \
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$folder/config/app.toml && \
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/$folder/config/app.toml && \
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$folder/config/app.toml
+
+sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 100/g' $HOME/$folder/config/config.toml
+sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 100/g' $HOME/$folder/config/config.toml
+
+snapshot_interval="1000" && \
+sed -i.bak -e "s/^snapshot-interval *=.*/snapshot-interval = \"$snapshot_interval\"/" $HOME/$folder/config/app.toml
