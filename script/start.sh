7bb2e771cd8ac18085de1a89166f1832ba881cc4
Dimokus <Dimokus@mail.ru>
2022-07-24 14:54:42 +0300

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # By Dimokus (https://t.me/Dimokus)
+TZ=Europe/Kiev
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+apt-get update
+apt-get upgrade -y
+apt-get install -y sudo nano wget tar zip unzip jq goxkcdpwgen ssh nginx build-essential git make gcc nvme-cli pkg-config libssl-dev libleveldb-dev clang bsdmainutils ncdu libleveldb-dev
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
 service ssh restart
