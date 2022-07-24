49ca2b22cdfae240744882c8bf727cddcdbbc306
Dimokus <Dimokus@mail.ru>
2022-07-24 09:37:28 +0300

    Update GoSetup.sh

--- script/GoSetup.sh
+++ script/GoSetup.sh
@@ -8,5 +8,5 @@ rm "go$ver.linux-amd64.tar.gz" && \
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
 source $HOME/.bash_profile && \
 go version
-sed -i.bak -e "s_secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"_secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/usr/local/go/bin:$HOME/go/bin"_;" /etc/sudoers
+sed -i.bak -e "s_ secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"_ secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/usr/local/go/bin:$HOME/go/bin"_;" /etc/sudoers
 mkdir -p /root/go/bin/
