a58a97f9886126de17e75d67863aeb32dfbdd06d
Dimokus <Dimokus@mail.ru>
2022-07-26 22:43:41 +0300

    Update toml_settings.sh

--- script/toml_settings.sh
+++ script/toml_settings.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # By Dimokus (https://t.me/Dimokus)
+source $HOME/.bashrc
 echo toml_settings.sh
 if [[ -n $link_peer ]]
 then
 	PEER=`curl -s $link_peer`
-	source $HOME/.bash_profile
+	source $HOME/.bashrc
 fi
 
 if [[ -n $link_seed ]]
 then
 	SEED=`curl -s $link_seed`
-	source $HOME/.bash_profile
+	source $HOME/.bashrc
 fi
 
 echo $PEER
@@ -35,5 +36,5 @@ sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 100/g' $HOME/$fold
 
 snapshot_interval="1000" && \
 sed -i.bak -e "s/^snapshot-interval *=.*/snapshot-interval = \"$snapshot_interval\"/" $HOME/$folder/config/app.toml
-source $HOME/.bash_profile
+source $HOME/.bashrc
 
