3bce574955befb00e0be77b6973b500fcee1c3cf
Dimokus <Dimokus@mail.ru>
2022-07-26 23:20:43 +0300

    Update GoSetup.sh

--- script/GoSetup.sh
+++ script/GoSetup.sh
@@ -6,7 +6,7 @@ sudo rm -rf /usr/local/go && \
 sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
 rm "go$ver.linux-amd64.tar.gz" && \
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> source $HOME/.bashrc && \
-source source $HOME/.bashrc && \
+source $HOME/.bashrc && \
 go version
 sed -i.bak -e "s~"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"~"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/usr/local/go/bin:$HOME/go/bin"~;" /etc/sudoers
 mkdir -p /root/go/bin/
