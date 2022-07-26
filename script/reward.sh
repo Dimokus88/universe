5772218b2e8558e92af84400133bcde096984b78
Dimokus <Dimokus@mail.ru>
2022-07-27 00:09:22 +0300

    Update reward.sh

--- script/reward.sh
+++ script/reward.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # By Dimokus (https://t.me/Dimokus)
 source $HOME/.bashrc
+reward=`$binary query distribution rewards $address $valoper -o json | jq -r .rewards[].amount`
+reward=`printf "%.f \n" $reward`
+echo ==============================
+echo ==Ваши награды: $reward $denom==
+echo ===Your reward $reward $denom===
+echo ==============================
+source $HOME/.bashrc
+sleep 5
 if [[ `echo $reward` -gt 1000000 ]]
 	then
 		echo =============================================================
@@ -11,7 +19,6 @@ if [[ `echo $reward` -gt 1000000 ]]
 		echo =============================================================
 		(echo ${PASSWALLET}) | $binary tx distribution withdraw-rewards $valoper --from $address --gas="auto" --fees 5555$denom --commission -y
 		reward=0
-		echo 'export reward='${reward} >> $HOME/.bashrc
 		sleep 5
 		source $HOME/.bashrc
 
