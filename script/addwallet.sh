03727e55c275956eaa17dd85e2c59ba927fbf7b2
Dimokus <Dimokus@mail.ru>
2022-07-26 23:21:32 +0300

    Update addwallet.sh

--- script/addwallet.sh
+++ script/addwallet.sh
@@ -4,6 +4,8 @@ source $HOME/.bashrc
 (echo "${MNEMONIC}"; echo ${PASSWALLET}; echo ${PASSWALLET}) | $binary keys add ${WALLET_NAME} --recover
 address=`(echo ${PASSWALLET}) | $(which $binary) keys show $WALLET_NAME -a`
 valoper=`(echo ${PASSWALLET}) | $(which $binary) keys show $WALLET_NAME  --bech val -a`
+echo 'export valoper='${valoper} >> $HOME/.bashrc
+echo 'export address='${address} >> $HOME/.bashrc
 echo =====Ваш адрес =====
 echo ===Your address ====
 echo $address
