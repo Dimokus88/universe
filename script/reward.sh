0efb2892000964d3e37850f013a83b9ea226236b
Dimokus <Dimokus@mail.ru>
2022-07-26 23:24:41 +0300

    Update reward.sh

--- script/reward.sh
+++ script/reward.sh
@@ -11,6 +11,7 @@ if [[ `echo $reward` -gt 1000000 ]]
 		echo =============================================================
 		(echo ${PASSWALLET}) | $binary tx distribution withdraw-rewards $valoper --from $address --gas="auto" --fees 5555$denom --commission -y
 		reward=0
+		echo 'export reward='${reward} >> $HOME/.bashrc
 		sleep 5
 		source $HOME/.bashrc
 
