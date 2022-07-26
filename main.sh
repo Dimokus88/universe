bbc9716b2ba7143d6507cc175d21ffe68f9aee18
Dimokus <Dimokus@mail.ru>
2022-07-26 22:46:49 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # By Dimokus (https://t.me/Dimokus)
+source $HOME/.bashrc
 curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/start.sh | bash
 curl -s https://raw.githubusercontent.com/Dimokus88/scripts/main/logo.sh | bash
 
@@ -9,6 +10,7 @@ curl -s https://raw.githubusercontent.com/Dimokus88/scripts/main/logo.sh | bash
 WORK (){
 while [[ $synh == false ]]
 do
+source $HOME/.bashrc
 curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/work.sh | bash
 done
 }
@@ -23,38 +25,45 @@ curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/GoSetup
 #-----------КОМПИЛЯЦИЯ БИНАРНОГО ФАЙЛА------------
 sudo curl -s  https://raw.githubusercontent.com/Dimokus88/universe/main/script/cbinary.sh | bash
 #-------------------------------------------------
+source $HOME/.bashrc
 echo ${PASSWALLET}
 echo ${WALLET_NAME}
-source $HOME/.bash_profile
+source $HOME/.bashrc
 sleep 2
 
 #=======ИНИЦИАЛИЗАЦИЯ БИНАРНОГО ФАЙЛА================
 echo =INIT=
+source $HOME/.bashrc
 rm /root/$folder/config/genesis.json
 $binary init "$MONIKER" --chain-id $chain --home /root/$folder
 sleep 5
 #====================================================
 
 #===========ДОБАВЛЕНИЕ КОШЕЛЬКА============
+source $HOME/.bashrc
 curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/addwallet.sh | bash
 #=========================================
 
 #===========ДОБАВЛЕНИЕ GENESIS.JSON===============
+source $HOME/.bashrc
 wget -O $HOME/$folder/config/genesis.json $genesis
 sha256sum ~/$folder/config/genesis.json
 cd && cat $folder/data/priv_validator_state.json
 #=================================================
 
 #===========ДОБАВЛЕНИЕ ADDRBOOK.JSON===============
+source $HOME/.bashrc
 rm $HOME/$folder/config/addrbook.json
 wget -O $HOME/$folder/config/addrbook.json $addrbook
 #==================================================
 
 # ------ПРОВЕРКА НАЛИЧИЯ priv_validator_key--------
+source $HOME/.bashrc
 curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/priv_validator_key_detection.sh | bash
 # -----------------------------------------------------------
 
 #-----ВНОСИМ ИЗМЕНЕНИЯ В CONFIG.TOML , APP.TOML.-----------
+source $HOME/.bashrc
 curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/toml_settings.sh | bash
 #-----------------------------------------------------------
 
@@ -65,19 +74,20 @@ curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/startup
 #===========ЗАПУСК НОДЫ============
 curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/run_at_service.sh | bash
 
-source $HOME/.bash_profile
+source $HOME/.bashrc
 sleep 20
 
 synh=`curl -s localhost:26657/status | jq .result.sync_info.catching_up`
 echo $synh
 tail -30 /var/log/$binary/current
 sleep 2
-source $HOME/.bash_profile
+source $HOME/.bashrc
 #==================================
 
 #=========Пока нода не синхронизирована - повторять===========
 while [[ $synh == true ]]
 do
+	source $HOME/.bashrc
 	echo ==============================================
 	echo =Нода не синхронизирована! Node is not sync! =
 	echo ==============================================
@@ -95,12 +105,13 @@ do
 	curl -s localhost:26657/status
 	synh=`curl -s localhost:26657/status | jq .result.sync_info.catching_up`
 	echo $synh
-	source $HOME/.bash_profile
+	source $HOME/.bashrc
 done
 
 #=======Если нода синхронизирована - начинаем работу ==========
 while	[[ $synh == false ]]
 do 	
+	source $HOME/.bashrc
 	sleep 10
 	date
 	echo ================================================================
@@ -109,20 +120,23 @@ do
 	val=`$binary query staking validator $valoper -o json | jq -r .description.moniker`
 	echo $val
 	echo $synh
-	source $HOME/.bash_profile
+	source $HOME/.bashrc
 	if [[ -z "$val" ]]
 	then
+		source $HOME/.bashrc
 		echo =Создание валидатора... Creating a validator...=
 		(echo ${PASSWALLET}) | $binary tx staking create-validator --amount="1000000$denom" --pubkey=$($binary tendermint show-validator) --moniker="$MONIKER"	--chain-id="$chain"	--commission-rate="0.10" --commission-max-rate="0.20" --commission-max-change-rate="0.01" --min-self-delegation="1000000" --gas="auto"	--from="$address" --fees="5550$denom" -y
 		sleep 20
 		val=`$binary query staking validator $valoper -o json | jq -r .description.moniker`
 		echo $val
-		source $HOME/.bash_profile
+		ssource $HOME/.bashrc
 	else
+		source $HOME/.bashrc
 		val=`$binary query staking validator $valoper -o json | jq -r .description.moniker`
 		echo $val
 		MONIKER=`echo $val`
 		WORK
-		source $HOME/.bash_profile
+		source $HOME/.bashrc
 	fi
+	source $HOME/.bashrc
 done
