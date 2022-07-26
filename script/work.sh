927e59e084d167e6484802a7450facffcd2f8fd2
Dimokus <Dimokus@mail.ru>
2022-07-27 00:09:32 +0300

    Update work.sh

--- script/work.sh
+++ script/work.sh
@@ -5,15 +5,7 @@ source $HOME/.bashrc
 	source $HOME/.bashrc
 
 	#===============СБОР НАГРАД И КОМИССИОННЫХ===================
-	reward=`$binary query distribution rewards $address $valoper -o json | jq -r .rewards[].amount`
-	reward=`printf "%.f \n" $reward`
-	echo 'export reward='${reward} >> $HOME/.bashrc
-	echo ==============================
-	echo ==Ваши награды: $reward $denom==
-	echo ===Your reward $reward $denom===
-	echo ==============================
-	source $HOME/.bashrc
-	sleep 5
+	
 	curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/reward.sh | bash
 	source $HOME/.bashrc
 
