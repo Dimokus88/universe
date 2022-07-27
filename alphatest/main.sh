cd382ce60a22d68fe8093250f5d07983bb4565a1
Dimokus <Dimokus@mail.ru>
2022-07-27 20:53:01 +0300

    Update main.sh

--- alphatest/main.sh
+++ alphatest/main.sh
@@ -36,13 +36,16 @@ echo  часть 1
 #-------------------------- Установка GO и кмопиляция бинарного файла -----------------------
 INSTALL (){
 # ----------УСТАНОВКА GO-----------
-ver="1.18.1" && \
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
-sudo rm -rf /usr/local/go && \
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
-rm "go$ver.linux-amd64.tar.gz" && \
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bashrc && \
-source $HOME/.bashrc && \
+ver="1.18.1" 
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" 
+sleep 5
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+echo OK
+sudo rm -rf /usr/local/go 
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" 
+rm "go$ver.linux-amd64.tar.gz" 
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bashrc 
+source $HOME/.bashrc 
 go version
 sed -i.bak -e "s~"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"~"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/usr/local/go/bin:$HOME/go/bin"~;" /etc/sudoers
 mkdir -p /root/go/bin/
