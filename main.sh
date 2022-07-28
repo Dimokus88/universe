8e2b53e01a4220adf7162c846acb11be413a7818
Dimokus <Dimokus@mail.ru>
2022-07-28 09:32:58 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -392,7 +392,7 @@ do
 	if [[ -z "$val" ]]
 	then		
 		echo =Создание валидатора... Creating a validator...=
-		(echo ${PASSWALLET}) | $binary tx staking create-validator --amount="1000000$denom" --pubkey=$($binary tendermint show-validator) --moniker="$MONIKER"	--chain-id="$chain" --commission-rate="0.10" --commission-max-rate="0.20" --commission-max-change-rate="0.01" --min-self-delegation="1000000" --gas="auto"	--from="$address" --fees="5550$denom" -y
+		(echo ${PASSWALLET}) | $binary tx staking create-validator --amount="1000000$denom" --pubkey=$($binary tendermint show-validator) --moniker="$MONIKER"	--chain-id="$chain" --commission-rate="0.10" --commission-max-rate="0.20" --commission-max-change-rate="0.01" --min-self-delegation="1000000" --gas="auto" --from="$address" --fees="5550$denom" -y
 		sleep 20
 		val=`$binary query staking validator $valoper -o json | jq -r .description.moniker`
 		echo $val
