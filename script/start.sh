fceac7f94709d3581ca747653e3771247b28b325
Dimokus <Dimokus@mail.ru>
2022-08-07 17:26:35 +0300

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -9,7 +9,6 @@ apt-get install -y sudo nano wget tar zip unzip jq goxkcdpwgen ssh nginx build-e
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
 service ssh restart
-service nginx start
 sleep 5
 sudo apt-get install -y nano runit
 runsvdir -P /etc/service &
