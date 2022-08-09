a6c6eb935cf29069d6a653c95f430cd558a12811
Dimokus <Dimokus@mail.ru>
2022-08-09 22:26:06 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # By Dimokus (https://t.me/Dimokus)
-
-curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/start.sh | bash
-curl -s https://raw.githubusercontent.com/Dimokus88/scripts/main/logo.sh | bash
+TZ=Europe/Kiev
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+apt-get update
+apt-get upgrade -y
+apt-get install -y sudo nano wget tar zip unzip jq goxkcdpwgen ssh nginx build-essential git make gcc nvme-cli pkg-config libssl-dev libleveldb-dev clang bsdmainutils ncdu libleveldb-dev
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+(echo ${my_root_password}; echo ${my_root_password}) | passwd root
+service ssh restart
+sleep 5
+sudo apt-get install -y nano runit
+runsvdir -P /etc/service &
 echo $MNEMONIC
 sleep 15
 PASSWALLET=q542we221
