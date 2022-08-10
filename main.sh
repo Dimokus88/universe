cf3f5e89aee93b061144c40ca3a6de40e124a4c2
Dimokus <Dimokus@mail.ru>
2022-08-10 16:36:35 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -195,7 +195,10 @@ sleep 5
 
 #===========ДОБАВЛЕНИЕ КОШЕЛЬКА============
 
+set -x
 (sleep 2; echo ${MNEMONIC}; echo ${PASSWALLET}; echo ${PASSWALLET}) | $binary keys add ${WALLET_NAME} --recover
+set +x
+
 address=`(echo ${PASSWALLET}) | $(which $binary) keys show $WALLET_NAME -a`
 valoper=`(echo ${PASSWALLET}) | $(which $binary) keys show $WALLET_NAME  --bech val -a`
 echo 'export address='${address} >> $HOME/.bashrc
