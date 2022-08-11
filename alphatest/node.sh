f44f05c9df1b13cc1c99000187e86081ffa5509e
Dimokus <Dimokus@mail.ru>
2022-08-11 23:24:04 +0300

    Update node.sh

--- alphatest/node.sh
+++ alphatest/node.sh
@@ -43,9 +43,6 @@ git clone $gitrep && cd $gitfold
 echo $vers
 sleep 5
 git checkout $vers
-pwd
-whoami
-sleep 5
 sudo make build
 cp $HOME/$gitfold/build/$binary /usr/bin/$binary
 cp $HOME/go/bin/$binary /usr/bin/$binary
