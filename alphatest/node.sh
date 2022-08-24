f8177e5cc2c608be7a361a075f889b814b5663e7
Dimokus <Dimokus@mail.ru>
2022-08-24 15:36:35 +0300

    Update node.sh

--- alphatest/node.sh
+++ alphatest/node.sh
@@ -398,7 +398,8 @@ synh=`curl -s localhost:26657/status | jq .result.sync_info.catching_up`
 	  if [[ -z "$val" ]]
 	  then		
 	  	  echo =Создание валидатора... Creating a validator...=
-	  	  (echo ${PASSWALLET}) | $binary tx staking create-validator --amount="1000000$denom" --pubkey=$($binary tendermint show-validator) --moniker="$MONIKER" --chain-id="$chain" --commission-rate="0.10" --commission-max-rate="0.20" --commission-max-change-rate="0.01" --min-self-delegation="1000000" --from="$address" --fees="5550$denom" -y
+		  DATE=`date`
+	  	  (echo ${PASSWALLET}) | $binary tx staking create-validator --amount="1000000$denom" --pubkey=$($binary tendermint show-validator) --moniker="$MONIKER" --chain-id="$chain" --commission-rate="0.10" --commission-max-rate="0.20" --commission-max-change-rate="0.01" --min-self-delegation="1000000" --from="$address" --details="Powered on Akash Network! Create $DATE" --fees="5550$denom" -y
 	  	  sleep 20
 	  	  val=`$binary query staking validator $valoper -o json | jq -r .description.moniker`
 	  	  echo $val
