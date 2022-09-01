34219d3853a05e4f8d0024cb214946b9b2b7c457
Dimokus <Dimokus@mail.ru>
2022-09-01 16:55:09 +0300

    Update create_validator.sh

--- script/create_validator.sh
+++ script/create_validator.sh
@@ -2,23 +2,27 @@
 source ~/.bashrc
 sleep 2
 if [ ! $MNEMONIC ]; then
-read -p "Enter node name: " MNEMONIC
+read -p "Enter mnemonic: " MNEMONIC
 fi
 sleep 2
 if [ ! $wallet_name ]; then
 read -p "Enter wallet name: " wallet_name
+echo 'export wallet_name ='${wallet_name } >> $HOME/.bashrc
 fi
 sleep 2
 if [ ! $pass_wallet ]; then
 read -p "Enter wallet password: " pass_wallet
+echo 'export pass_wallet='${pass_wallet} >> $HOME/.bashrc
 fi
 echo == Импортирую кошелек ==
 (echo ${MNEMONIC}; echo ${pass_wallet}; echo ${pass_wallet}) | $binary keys add ${wallet_name} --recover
 sleep 2
 echo == Запрашиваю баланс ==
-address=`$binary keys show $WALLET_NAME -a | sed -e "s_/root/.__;"`
+address=`$binary keys show $wallet_name -a | sed -e "s_/root/.__;"`
+echo 'export address='${address} >> $HOME/.bashrc
 sleep 2
-valoper=`$binary keys show $WALLET_NAME --bech val -a | sed -e "s_/root/.__;"`
+valoper=`$binary keys show $wallet_name --bech val -a | sed -e "s_/root/.__;"`
+echo 'export valoper='${valoper} >> $HOME/.bashrc
 sleep 2
 balance=`$binary q bank balances $address -o json | jq -r .balances[0].amount `
 sleep 2
@@ -35,7 +39,7 @@ sleep 2
 echo == Создаю валидатора ==
 sleep 2
 DATE=`date`
-(echo ${PASSWALLET}) | $binary tx staking create-validator --amount="1000000$denom" --pubkey=$($binary tendermint show-validator) --moniker="$MONIKER" --chain-id="$chain" --commission-rate="0.10" --commission-max-rate="0.20" --commission-max-change-rate="0.01" --min-self-delegation="1000000" --from="$address" --details="Powered on Akash Network! Create $DATE" --fees="5550$denom" --gas="auto" -y
+(echo ${pass_wallet}) | $binary tx staking create-validator --amount="1000000$denom" --pubkey=$($binary tendermint show-validator) --moniker="$MONIKER" --chain-id="$chain" --commission-rate="0.10" --commission-max-rate="0.20" --commission-max-change-rate="0.01" --min-self-delegation="1000000" --from="$address" --details="Powered on Akash Network! Create $DATE" --fees="5550$denom" --gas="auto" -y
 sleep 10
 val=`$binary query staking validator $valoper -o json | jq -r .description.moniker`
 if [[ -z "$val" ]]
