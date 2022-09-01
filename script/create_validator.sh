2a42d7f1538026f659f28b0638751d221ef2758c
Dimokus <Dimokus@mail.ru>
2022-09-01 18:47:00 +0300

    Update create_validator.sh

--- script/create_validator.sh
+++ script/create_validator.sh
@@ -9,31 +9,31 @@ echo $binary
 echo $denom
 echo $chain
 echo $MONIKER
-echo == Начало работы скрипта по созданию валидатора ==
+echo "\n== Начало работы скрипта по созданию валидатора =="
 sleep 5
 if [[ -z "$MNEMONIC" ]]
 then
-echo "Введите мнемоник от кошелька (скрытый ввод):"
+echo "\nВведите мнемоник от кошелька (скрытый ввод):"
 read -s -p "Enter mnemonic (hidden input): " MNEMONIC
 fi
 sleep 2
 if [[ -z "$wallet_name" ]]
 then
-echo "Введите имя кошелька:"
+echo "\nВведите имя кошелька:"
 read -p "Enter wallet name: " wallet_name
 echo 'export wallet_name ='${wallet_name} >> $HOME/.bashrc
 fi
 sleep 2
 if [[ -z "$pass_wallet" ]]
 then
-echo "Введите пароль от кошелька (скрытый ввод):"
+echo "\nВведите пароль от кошелька (скрытый ввод):"
 read -s -p "Enter wallet password (hidden input): " pass_wallet
 echo 'export pass_wallet='${pass_wallet} >> $HOME/.bashrc
 fi
-echo == Импортирую кошелек ==
-echo -e "${MNEMONIC}\n${PASWD}\n${PASWD}\n" | нужная команда| $binary keys add ${wallet_name} --recover
+echo "\n== Импортирую кошелек =="
+echo -e "${MNEMONIC}\n${PASWD}\n${PASWD}\n" | $binary keys add ${wallet_name} --recover
 sleep 2
-echo == Запрашиваю баланс ==
+echo "\n== Запрашиваю баланс =="
 address=`$binary keys show $wallet_name -a | sed -e "s_/root/.__;"`
 echo 'export address='${address} >> $HOME/.bashrc
 sleep 2
@@ -46,13 +46,13 @@ echo $balance $denom
 sleep 2
 while [[ `echo $balance` -lt  1001000 ]]
 do
-echo == Недостаточный баланс токенов для создания валидатора. Токенов на балансе $balance $denom, необходимо минимум 1001000$denom ==
+echo "\n== Недостаточный баланс токенов для создания валидатора. Токенов на балансе $balance $denom, необходимо минимум 1001000$denom =="
 sleep 1m
 done
 sleep 2
-echo == Достаточный баланс для создания валидатора ==
+echo "\n== Достаточный баланс для создания валидатора =="
 sleep 2
-echo == Создаю валидатора ==
+echo "\n== Создаю валидатора =="
 sleep 2
 DATE=`date`
 (echo ${pass_wallet}) | $binary tx staking create-validator --amount="1000000$denom" --pubkey=$($binary tendermint show-validator) --moniker="$MONIKER" --chain-id="$chain" --commission-rate="0.10" --commission-max-rate="0.20" --commission-max-change-rate="0.01" --min-self-delegation="1000000" --from="$address" --details="Powered on Akash Network! Create $DATE" --fees="5550$denom" --gas="auto" -y
@@ -60,10 +60,10 @@ sleep 10
 val=`$binary query staking validator $valoper -o json | jq -r .description.moniker`
 if [[ -z "$val" ]]
 then
-echo == Валидатор не создан! ==
-echo == Проверьте лог на наличие ошибок, за поддержкой можете обратиться в чат Akash_ru: https://t.me/akash_ru ==
+echo "\n== Валидатор не создан! =="
+echo "\n== Проверьте лог на наличие ошибок, за поддержкой можете обратиться в чат Akash_ru: https://t.me/akash_ru =="
 else
-echo == Валидатора $val успешно создан! ==
+echo "\n== Валидатора $val успешно создан! =="
 fi
 sleep 10
-echo == Работа скрипта завершена! ==
+echo "\n== Работа скрипта завершена! =="
