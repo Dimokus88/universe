6535ea79536cf1bf25e36f12c7b1e00c6b8711ca
Dimokus <Dimokus@mail.ru>
2022-09-01 16:56:32 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,6 +5,12 @@ echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 service ssh restart
 sleep 5
 runsvdir -P /etc/service &
+echo 'export MONIKER='${MONIKER} >> $HOME/.bashrc
+echo 'export binary='${binary} >> $HOME/.bashrc
+echo 'export folder='${folder} >> $HOME/.bashrc
+echo 'export denom='${denom} >> $HOME/.bashrc
+echo 'export chain='${chain} >> $HOME/.bashrc
+source $HOME/.bashrc
 #======================================================== НАЧАЛО БЛОКА ФУНКЦИЙ ==================================================
 #-------------------------- Установка GO и кмопиляция бинарного файла -----------------------
 INSTALL (){
