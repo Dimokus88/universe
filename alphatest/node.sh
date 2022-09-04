766cf7accd652a44e6544954faa029104c211335
Dimokus <Dimokus@mail.ru>
2022-09-04 13:15:18 +0300

    Update node.sh

--- alphatest/node.sh
+++ alphatest/node.sh
@@ -53,7 +53,7 @@ then
 n_peers=`curl -s $SNAP_RPC/net_info? | jq -r .result.n_peers`
 let n_peers="$n_peers"-1
 RPC="$SNAP_RPC"
-echo "$RPC," >> /root/RPC.txt
+echo -n "$RPC," >> /root/RPC.txt
 p=0
 echo "Search peers..."
 while [[ "$p" -le  "$n_peers" ]]
@@ -62,12 +62,12 @@ do
         if [[ ! "$PEER" =~ "tcp" ]] 
         then
 			id=`curl -s  $SNAP_RPC/net_info? | jq -r .result.peers["$p"].node_info.id`
-            echo "$id@$PEER," >> /root/PEER.txt
-			echo "peer"
-			echo $PEER
+            		echo -n "$id@$PEER," >> /root/PEER.txt
+			echo "Find peer"
+			echo $id@$PEER
 			rm /root/addr.tmp
 			echo $PEER | sed 's/:/ /g' > /root/addr.tmp
-			ADDRESS=(`cat addr.tmp`)
+			ADDRESS=(`cat /root/addr.tmp`)
 			ADDRESS=`echo ${ADDRESS[0]}`
 			PORT=(`cat /root/addr.tmp`)
 			PORT=`echo ${PORT[1]}`
@@ -77,10 +77,8 @@ do
 			if [[ `curl -s http://$RPC/abci_info? --connect-timeout 5 | jq -r .result.response.last_block_height` -gt 0 ]]
 			then
 				echo "rpc"
-				echo "$RPC," >> /root/RPC.txt
+				echo -n "$RPC," >> /root/RPC.txt
 				RPC=0
-			else
-			echo "rpc not found"
 			fi
 			RPC=0
    	     fi
