24c1555335becb83e1fcd14892672a41c30d149d
Dimokus <Dimokus@mail.ru>
2022-09-14 17:34:28 +0300

    Update alert_bot.sh

--- script/alert_bot.sh
+++ script/alert_bot.sh
@@ -11,7 +11,7 @@ echo -e "\n\xF0\x9F\x92\xBBHardware status:" >>/root/bot/text.txt
 echo - CPU used: `ps -aux | grep -m1 "$binary start" | awk '{print $3}'`% >> /root/bot/text.txt
 echo - RAM used: `ps -aux | grep -m1 "$binary start" | awk '{print $4}'`% >> /root/bot/text.txt
 echo  -Memory used: `du /root/ -sh |sed "s_G_GiB_" |sed "s_/root/__"` >> /root/bot/text.txt
-TOKEN='5413800716:AAHAwfZgg-c02OD3hYDH_QD8QwLKBEE-VL4'
+TOKEN='5611980475:AAGkj6n4QhmLF8GOhWFOeM3cs9sCuIvqTCI'
 echo $CHAT_ID $TOKEN
 DATE=`date`
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
