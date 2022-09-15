06ef418bed6db2fd5b0d921eb1bd8b42aaf5e2ab
Dimokus <Dimokus@mail.ru>
2022-09-15 11:39:16 +0300

    Update node.sh

--- alphatest/node.sh
+++ alphatest/node.sh
@@ -247,6 +247,30 @@ chmod +x /root/bot/log/run
 ln -s /root/bot /etc/service
 sleep 5
 echo == Оповещение Telegram включено ==
+sleep 5
+echo == Установка cheker proposal ==
+mkdir /root/tmp/
+mkdir /root/cheker/
+wget -O /root/cheker/cheker_proposal.sh https://raw.githubusercontent.com/Dimokus88/universe/main/script/cheker_proposal.sh && chmod +x /root/cheker/cheker_proposal.sh
+mkdir /root/cheker/log
+sleep 5  
+cat > /root/cheker/run <<EOF 
+#!/bin/bash
+exec 2>&1
+exec /root/cheker/cheker_proposal.sh $binary $TOKEN $CHAT_ID 
+EOF
+chmod +x /root/cheker/run
+LOG=/var/log/cheker
+
+cat > /root/cheker/log/run <<EOF 
+#!/bin/bash
+mkdir $LOG
+exec svlogd -tt $LOG
+EOF
+chmod +x /root/cheker/log/run
+ln -s /root/cheker /etc/service
+sleep 5
+echo == Cheker proposal установлен ==
 fi
 #==============================
 sleep 1m
