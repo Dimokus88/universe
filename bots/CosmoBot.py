bf6712df64d18c6dea57d8ed31fd889145aec4e3
Dimokus <Dimokus@mail.ru>
2022-09-16 19:48:17 +0300

    Create CosmoBot.py

---
+++ bots/CosmoBot.py
@@ -0,0 +1,21 @@
+import telebot
+from telebot import types
+import os
+import subprocess
+bot = telebot.TeleBot("5531416249:AAHgz_ag3bXMutra6z8Us_adB5TwlMK_1wQ")
+binary = os.getenv('bunary')
+@bot.message_handler(commands=['start'])
+def start_message(message):
+        bot.send_message(message.chat.id,"Welcome to Akash Nodes Alert Bot!")
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Status")
+        markup.add(item1)
+        bot.send_message(message.chat.id,"Select functions please!",reply_markup=markup)
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+        if message.text == "Status":
+                subprocess.check_call("/root/bot/script.sh '%s'" % binary, shell=True)
+                text = open ('/root/bot/text.txt')
+                bot.send_message(message.chat.id,text.read())
+bot.infinity_polling()
