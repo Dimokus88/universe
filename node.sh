2b830114a01afa0c51aff0e9d22954464ff1dfb3
Dimokus <Dimokus@mail.ru>
2022-10-19 21:54:31 +0300

    Update node.sh

--- node.sh
+++ node.sh
@@ -100,7 +100,7 @@ echo -n "$RPC," >> /root/RPC.txt
 p=0
 count=0
 echo "Search peers..."
-while [[ "$p" -le  "$n_peers" ]] && [[ "$count" -le  14 ]]
+while [[ "$p" -le  "$n_peers" ]] && [[ "$count" -le  29 ]]
 do
 	PEER=`curl -s  $SNAP_RPC/net_info? | jq -r .result.peers["$p"].node_info.listen_addr`
         if [[ ! "$PEER" =~ "tcp" ]] 
