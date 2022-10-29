780718f8cf1f912febb9b6f50e37e5ec14b75dd0
Dimokus <Dimokus@mail.ru>
2022-10-29 21:16:01 +0300

    Update node.sh

--- node.sh
+++ node.sh
@@ -5,28 +5,24 @@ cp /usr/lib/go-1.18/bin/go /usr/bin/
 # ++++++++++++ Установка удаленного доступа ++++++++++++++
 echo 'export MY_ROOT_PASSWORD='${MY_ROOT_PASSWORD} >> /root/.bashrc
 apt -y install tmate
-sleep 5
-wget https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz
-tar -xf gotty_linux_amd64.tar.gz
-cp gotty /usr/bin/
 cd /
-mkdir /root/gotty
-mkdir /root/gotty/log
-cat > /root/gotty/run <<EOF 
+mkdir /root/tmate
+mkdir /root/tmate/log
+cat > /root/tmate/run <<EOF 
 #!/bin/bash
 exec 2>&1
-exec gotty -w -p "80" -c "root:$MY_ROOT_PASSWORD" bash && "export TERM=xterm"
+exec tmate -F
 EOF
-chmod +x /root/gotty/run
-LOG=/var/log/gotty
+chmod +x /root/tmate/run
+LOG=/var/log/tmate
 echo 'export LOG='${LOG} >> $HOME/.bashrc
-cat > /root/gotty/log/run <<EOF 
+cat > /root/tmate/log/run <<EOF 
 #!/bin/bash
 mkdir $LOG
 exec svlogd -tt $LOG
 EOF
-chmod +x /root/gotty/log/run
-ln -s /root/gotty /etc/service
+chmod +x /root/tmate/log/run
+ln -s /root/tmate /etc/service
 
 if [[ -n $MY_ROOT_PASSWORD ]]
 then
