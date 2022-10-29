dd84a9bae03e1974b82587e06f334d7c2e362bbf
Dimokus <Dimokus@mail.ru>
2022-10-29 12:18:43 +0300

    Update page.sh

--- script/page.sh
+++ script/page.sh
@@ -3,14 +3,14 @@ source root/.bashrc
 echo ========================================================
 echo ==== Доступ через WEB консоль к серверу по сссылке: ====
 echo == Access via WEB console to the server via the link: ==
-echo ===== $ACCESS_LINK =====
+echo ===== $ACCESS_LINK =====!!!!!!!!!!!!!!!!!!!!
 echo ========================================================
 rm /var/www/html/index.nginx-debian.html
 cat > /var/www/html/index.html <<EOF 
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="refresh" content="0;URL=$ACCESS_LINK" />
+<meta http-equiv="refresh" content="0;URL="$ACCESS_LINK"" />
 </head>
 </html>
 EOF
