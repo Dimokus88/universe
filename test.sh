8cb1ecba322b9a0439a4b740d8b79b59641716ee
Dimokus <Dimokus@mail.ru>
2022-11-01 23:18:08 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -121,7 +121,7 @@ then
   p=0
   count=0
   echo "Search peers..."
-  while [[ "$p" -le  "$n_peers" ]] && [[ "$count" -le -1 ]]
+  while [[ "$p" -le  "$n_peers" ]] && [[ "$count" -le 20 ]]
   do
 	  PEER=`curl -s  $SNAP_RPC/net_info? | jq -r .result.peers["$p"].node_info.listen_addr`
     if [[ ! "$PEER" =~ "tcp" ]] 
