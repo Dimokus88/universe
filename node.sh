7f5b11130b6d35a05bb828b66252178ce796b25f
Dimokus <Dimokus@mail.ru>
2022-11-08 20:09:48 +0300

    Update node.sh

--- node.sh
+++ node.sh
@@ -113,13 +113,20 @@ then
   echo "Search peers..."
   while [[ "$p" -le  "$n_peers" ]] && [[ "$count" -le 9 ]]
   do
-  PEER=`curl -s  $SNAP_RPC/net_info? | jq -r .result.peers["$p"].url`
+	  PEER=`curl -s  $SNAP_RPC/net_info? | jq -r .result.peers["$p"].node_info.listen_addr`
     if [[ ! "$PEER" =~ "tcp" ]] 
     then
-         echo -n "$PEER," >> /root/PEER.txt
-         echo $PEER
-     fi
-p="$p"+1
+    	id=`curl -s  $SNAP_RPC/net_info? | jq -r .result.peers["$p"].node_info.id`
+   		echo -n "$id@$PEER," >> /root/PEER.txt
+			echo $id@$PEER
+			rm /root/addr.tmp
+			echo $PEER | sed 's/:/ /g' > /root/addr.tmp
+			ADDRESS=(`cat /root/addr.tmp`)
+			ADDRESS=`echo ${ADDRESS[0]}`
+			PORT=(`cat /root/addr.tmp`)
+			PORT=`echo ${PORT[1]}`			
+   	fi
+	p="$p"+1
 done
 echo "Search peers is complete!"
 PEER=`cat /root/PEER.txt | sed 's/,$//'`
