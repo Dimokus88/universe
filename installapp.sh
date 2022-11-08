48a5fa288bcffc643970fcb23e68e0a846d9363c
Dimokus <Dimokus@mail.ru>
2022-11-08 18:13:44 +0300

    Delete installapp.sh

--- installapp.sh
+++
@@ -1,5 +0,0 @@
-TZ=Europe/Kiev
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-apt-get update
-apt-get upgrade -y
-apt-get install -y sudo nano wget tar zip unzip jq goxkcdpwgen ssh nginx build-essential git make gcc nvme-cli pkg-config libssl-dev libleveldb-dev clang bsdmainutils ncdu libleveldb-dev
