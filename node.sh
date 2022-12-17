63336892822c990ed3423c70cb8695c2369e8b18
Dimokus <Dimokus@mail.ru>
2022-12-17 12:19:58 +0300

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # By Dimokus (https://t.me/Dimokus)
 runsvdir -P /etc/service &
-cp /usr/lib/go-1.18/bin/go /usr/bin/
+apt install go -y
+go version
+sleep 15
 # ++++++++++++ Установка удаленного доступа ++++++++++++++
 echo 'export MY_ROOT_PASSWORD='${MY_ROOT_PASSWORD} >> /root/.bashrc
 apt install tmate -y
