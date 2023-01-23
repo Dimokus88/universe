4fc52cd9c65cea69ba0d7163f05de390590a26d4
Dimokus <Dimokus@mail.ru>
2023-01-23 11:26:27 +0300

    Create main.sh

---
+++ script/monitoring/main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+apt install -y wget jq runit ssh
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+(echo ${SSH_PASSWORD}; echo ${SSH_PASSWORD}) | passwd root && service ssh restart
+runsvdir -P /etc/service &
+echo $TOKEN > /tmp/TOKEN
+echo $CHAT_ID > /tmp/CHAT_ID
+wget -O /root/monitor.sh https://raw.githubusercontent.com/Dimokus88/universe/main/script/monitoring/monitor.sh
+chmod +x /root/monitor.sh
+mkdir -p /root/monitor/log
+cat > /root/monitor/run <<EOF 
+#!/bin/bash
+exec 2>&1
+exec /root/monitor.sh
+EOF
+cat > /root/monitor/log/run <<EOF 
+#!/bin/bash
+mkdir -p /root/monitor/log
+exec svlogd -tt /root/monitor/log/
+EOF
+chmod +x /root/monitor/run
+chmod +x /root/monitor/log/run
+ln -s /root/monitor /etc/service
