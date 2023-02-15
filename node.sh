44f235cbfcec4bb420bc42ecad36e0b21404f810
Dimokus <Dimokus@mail.ru>
2023-02-15 14:35:11 +0300

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # By Dimokus (https://t.me/Dimokus)
 runsvdir -P /etc/service &
+rm -r /usr/local/go
 wget https://go.dev/dl/go1.20.1.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.20.1.linux-amd64.tar.gz
 echo 'export PATH='$PATH:/usr/local/go/bin >> /root/.bashrc
 go version
