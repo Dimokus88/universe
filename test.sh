aa28cf9db30858bf4b33c631d2c4d7f2a2969df0
Dimokus <Dimokus@mail.ru>
2023-02-15 17:19:37 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Часть 1 Установка ПО
 TZ=Europe/Kiev && ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-apt install -y nano tar wget lz4 zip jq ssh runit build-essential git make gcc 
+apt install -y nano tar wget lz4 zip jq ssh runit build-essential git make gcc nvme-cli
 runsvdir -P /etc/service &
 wget https://go.dev/dl/go1.20.1.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.20.1.linux-amd64.tar.gz
 PATH=$PATH:/usr/local/go/bin
