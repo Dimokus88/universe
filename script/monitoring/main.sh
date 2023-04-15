5943a778bdcabfc6a25b2fda4066d0bf3ae37df3
Dimokus <Dimokus@mail.ru>
2023-04-15 23:26:41 +0300

    Update main.sh

--- script/monitoring/main.sh
+++ script/monitoring/main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt install -y wget jq runit ssh
+apt install -y wget jq runit ssh bc
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 (echo ${SSH_PASSWORD}; echo ${SSH_PASSWORD}) | passwd root && service ssh restart
 runsvdir -P /etc/service &
