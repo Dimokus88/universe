9b7f267dfe3e9d359cc3ee1ade3f6c235f7a8c7d
Dimokus <Dimokus@mail.ru>
2023-04-17 23:06:01 +0300

    Update report.sh

--- script/monitoring/report.sh
+++ script/monitoring/report.sh
@@ -31,7 +31,7 @@ STATUS=`cat ~/monitor/base.json | jq -r .[$p].status`
  CHANGE_PERCENT=$(awk "BEGIN {print (($LATEST_VOTING_POWER-$VOTING_POWER)/$VOTING_POWER)*100}")
  PRICE=`curl -s $LINK_PRICE | jq .market_data.current_price.usd`
  DELEGATE_USD=$(printf "%.0f" $(echo "$VOTING_POWER * $PRICE" | bc))
- TEXT="\`\`\`Проект: **"$PROJECT"**\nВсего заделегировано токенов: **"$VOTING_POWER"**\nИзменение в делегации: **"$CHANGE"** токенов или **"$CHANGE_PERCENT"%**\nЦена "$PROJECT" на Coingecko: **"$PRICE"\$**\nЗаделегировано в USD: **"$DELEGATE_USD"\$**\n \`\`\`"
+ TEXT="Проект: **"$PROJECT"**\nВсего заделегировано токенов: **"$VOTING_POWER"**\nИзменение в делегации: **"$CHANGE"** токенов или **"$CHANGE_PERCENT"%**\nЦена "$PROJECT" на Coingecko: **"$PRICE"\$**\nЗаделегировано в USD: **"$DELEGATE_USD"\$**\n "
  TEXT=$(echo -e "${TEXT}" | jq -Rs .)
  echo $TEXT
  echo $URL
