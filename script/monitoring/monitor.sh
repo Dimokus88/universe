49b2602343ffcf29d9c07bd23dc33f84a5a7a5e4
Dimokus <Dimokus@mail.ru>
2023-04-18 17:24:56 +0300

    Update monitor.sh

--- script/monitoring/monitor.sh
+++ script/monitoring/monitor.sh
@@ -2,6 +2,7 @@
 URL=`cat /tmp/URL`
 p=0
 curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/monitoring/base.json > ~/monitor/base.json
+wget -P /root/monitor/ https://raw.githubusercontent.com/Dimokus88/universe/main/script/monitoring/emoji.json
 crontab -l > current_cron
 echo "0 10 * * * curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/monitoring/report.sh | bash " >> ~/monitor/current_cron
 crontab -l | cat - ~/monitor/current_cron | crontab -
@@ -31,7 +32,8 @@ LAST_BLOCK=\`curl -s \$RPC/abci_info? | jq -r .result.response.last_block_height
 while [[ -z \$LAST_BLOCK ]]
 do
 LAST_BLOCK=\`curl -s \$RPC/abci_info? | jq -r .result.response.last_block_height\`
-echo :cold_sweat: Внимание! RPC нода \$PROJECT недоступна !
+EMOJI=\$(cat /root/monitor/emoji.json | jq -r .alarm[] | shuf -n 1)
+echo $EMOJI Внимание! RPC нода \$PROJECT недоступна !
 sleep 10m
 curl -s https://raw.githubusercontent.com/Dimokus88/universe/main/script/monitoring/base.json > ~/monitor/base.json
 done
@@ -60,7 +62,8 @@ done
 
 if [ "\$NOSIGN" -gt "\$SIGN" ]
 then
-curl -H "Content-Type: application/json" -X POST -d '{"content":"Node $PROJECT Alert $USER ! :scream: \\nВнимание! Нода пропускает блоки! \\n**Срочно проверьте работоспособность!** \\n\`\`\`Подписано '\$SIGN' блоков из 19. \\nНе подписано '\$NOSIGN' блоков из 19. \`\`\`"}' $URL
+EMOJI=\$(cat /root/monitor/emoji.json | jq -r .panic[] | shuf -n 1)
+curl -H "Content-Type: application/json" -X POST -d '{"content":"\$EMOJI Node $PROJECT Alert $USER !\\nВнимание! Нода пропускает блоки! \\n**Срочно проверьте работоспособность!** \\n\`\`\`Подписано '\$SIGN' блоков из 19. \\nНе подписано '\$NOSIGN' блоков из 19. \`\`\`"}' $URL
 fi
 
 sleep 10m
@@ -68,7 +71,8 @@ done
 EOF
 chmod +x ~/monitor/$PROJECT/$PROJECT.sh
 echo Создан $PROJECT.sh !
-curl -s -H "Content-Type: application/json" -X POST -d '{"content":"Создано оповещение на '$PROJECT' для '$USER' :wink: ."}' $URL
+EMOJI=$(cat /root/monitor/emoji.json | jq -r .happy[] | shuf -n 1)
+curl -s -H "Content-Type: application/json" -X POST -d '{"content":"$EMOJI Создано оповещение на '$PROJECT' для '$USER' ."}' $URL
 cat > ~/monitor/$PROJECT/run <<EOF 
 #!/bin/bash
 exec 2>&1
